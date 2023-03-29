8309154ccc8ddb8564f8c6145cb6a5d6cba216f0
Radhika Patel <radhithakkar.02@gmail.com>
2023-03-29 14:25:10 -0400

    changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -195,7 +195,7 @@
     <!-- /.content-wrapper -->
     
     <footer class="main-footer">
-      <strong>Copyright ©  2022. Designed by :   <a href="https://thesanfinity.com/" target="_blank">Sanfinity Creative Solution</a>.</strong>
+      <strong>Copyright ©  2022. Designed by :   <a href="javascript:void(0) target="_blank">Maintenance System</a>.</strong>
       All rights reserved.
       <div class="float-right d-none d-sm-inline-block">
         <b>Version</b> 1.1.0
--- resources/views/theme/sidebar.blade.php
+++ resources/views/theme/sidebar.blade.php
@@ -1,7 +1,7 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
   <!-- Brand Logo -->
   <a href="{{ route('home') }}" class="brand-link">
-    <img src="{{URL::asset('/theme/dist/images/logo.png')}}" alt="Sanfinity Logo" class="brand-image">
+    <img src="{{URL::asset('/theme/dist/images/logo.png')}}" alt="Logo" class="brand-image">
   </a>
   @php
   $img="Admin/no_preview.jpg";
